e5c887adafa6600a1407a501787e80914478f695
Henrique Pereira <henriquepereiraemp@gmail.com>
2026-04-21 12:46:45 -0400

    refatorando código

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native"
 export default function Home(){
     return(
         <View style={styles.container}>
-            <Text>Home</Text>
+            <Text>Tela Home</Text>
         </View>
     )
 }
